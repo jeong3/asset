2ff47e2a59dcea4889c7970a1eaec8847ce3ba83
jeong3 <129027311+jeong3@users.noreply.github.com>
2024-12-18 14:55:01 +0900

    인덱스 설정 수정

--- asset/src/main/java/springBootMVCAsset/controller/EmployeeController.java
+++ asset/src/main/java/springBootMVCAsset/controller/EmployeeController.java
@@ -2,12 +2,14 @@ package springBootMVCAsset.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("employee")
 public class EmployeeController {
 	@GetMapping("employeeList")
 	public String employeeList() {
 		
-		return "thymeleaf/empList";
+		return "thymeleaf/emp/empList";
 	}
 }
