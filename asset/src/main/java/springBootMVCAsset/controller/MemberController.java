10ac54337137c2da36633cae9650015cccb8ff0d
yugang1122 <138686751+yugang1122@users.noreply.github.com>
2024-12-18 17:53:19 +0900

    인덱스+멤버 등록중

--- asset/src/main/java/springBootMVCAsset/controller/MemberController.java
+++ asset/src/main/java/springBootMVCAsset/controller/MemberController.java
@@ -2,9 +2,11 @@ package springBootMVCAsset.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import springBootMVCAsset.command.MemberCommand;
 import springBootMVCAsset.service.AutoNumService;
 
 @Controller
@@ -14,8 +16,11 @@ public class MemberController {
 	AutoNumService autoNumService;
 	
 	@GetMapping("memberRegist")
-	public String memberRegist() {
+	public String memberRegist(Model model) {
 		//String autoNum = autoNumService.execute("mem_", "member_num", 5, "members");
+		//MemberCommand memberCommand = new MemberCommand();
+		//memberCommand.setMemberNum(autoNum);
+		//model.addAttribute("memberCommand", memberCommand);
 		return "thymeleaf/member/memberRegist";
 	}
 }
