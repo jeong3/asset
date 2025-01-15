92a30c16295a2406ba47736797b9d184866ca65e
yugang1122 <138686751+yugang1122@users.noreply.github.com>
2025-01-15 14:41:15 +0900

    카카오 로그인시 버젯 생성되게

--- asset/src/main/java/springBootMVCAsset/service/MemberService.java
+++ asset/src/main/java/springBootMVCAsset/service/MemberService.java
@@ -3,14 +3,20 @@ package springBootMVCAsset.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import springBootMVCAsset.command.MemberCommand;
 import springBootMVCAsset.domain.AuthInfoDTO;
 import springBootMVCAsset.domain.KakaoUser;
 import springBootMVCAsset.mapper.MemberMapper;
+import springBootMVCAsset.service.budget.BudgetRegistService;
 
 @Service
 public class MemberService {
 	@Autowired
 	MemberMapper memberMapper;
+	@Autowired
+	AutoNumService autoNumService;
+	@Autowired
+	BudgetRegistService budgetRegistService;
 	public AuthInfoDTO findOrCreateMember(KakaoUser kakaoUser) {
         // 카카오 ID로 사용자 검색
         AuthInfoDTO auth = memberMapper.findByKakaoId(kakaoUser.getId());
@@ -23,8 +29,17 @@ public class MemberService {
             AuthInfoDTO newAuth = new AuthInfoDTO();
             newAuth.setUserId(kakaoUser.getId());
             // 사용자 저장
+      
+           String autoNum = autoNumService.execute("mem_", "member_num", 5, "members");
+           newAuth.setUserNum(autoNum);
            memberMapper.KakaoAuthSave(newAuth);
-            return newAuth;
+           String memberNum = newAuth.getUserNum();
+           MemberCommand memberCommand = new MemberCommand();
+           memberCommand.setMemberNum(memberNum);
+           String budgetNum = autoNumService.execute("budget_", "budget_num", 8, "budget");
+           budgetRegistService.execute(memberCommand, budgetNum);
+           
+           return newAuth;
         }
     }
 }
