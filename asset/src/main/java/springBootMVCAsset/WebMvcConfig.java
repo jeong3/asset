2c45c2bdaac7ec0ed54c77a1f8b21a95850b8fd5
jeong3 <129027311+jeong3@users.noreply.github.com>
2025-01-16 11:10:55 +0900

    인터셉터 추가 및 인터셉터 걸리면 로그인이 필요하다는 알림이뜨면서 로그인페이지로 이동

---
+++ asset/src/main/java/springBootMVCAsset/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package springBootMVCAsset;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	@Autowired
+	InteceptorConfig inteceptorConfig;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//로그인 하지 않아도 되는 페이지 저장
+		List<String> excludeList = new ArrayList<String>();
+		excludeList.add("/news/**/*");
+		excludeList.add("/login/**/*");
+		excludeList.add("/goods/**/*");
+		excludeList.add("/static/**/*");
+		excludeList.add("/announce/**/*");
+		excludeList.add("/inquire/**/*");
+		excludeList.add("/volunteer/**/*");
+		excludeList.add("/stock/**/*");
+		registry.addInterceptor(inteceptorConfig)
+				.addPathPatterns("/**/*") // 모두 차단
+				.excludePathPatterns(excludeList); // 허용할 주소
+	}
+}
